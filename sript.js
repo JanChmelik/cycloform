850752c77ef5a7c847a392247ff4360f4b686a73
Jan <chmelikjan@atlas.cz>
2024-02-21 12:00:41 +0100

    one of radioes defaultly selected; alert if no bicycle selected in PriceCalcultaion and in Reservation

--- sript.js
+++ sript.js
@@ -8,6 +8,7 @@ const dobaZapujcky = document.querySelector('select[name="sel-dny"]');
 const nosic = document.querySelectorAll(`input[name="rd-nosic"]`);
 const rozpocet = document.querySelector('input[name="navrhCeny"]');
 let cenaZapujcky;
+let isAtLeastOneBicycleSelected = false;
 //------------------------- odeslani reservace
 const btnRezerve = document.querySelector(`button[name=reserve]`);
 const email = document.querySelector('input[type="email"]');
@@ -17,6 +18,7 @@ btnVypocti.addEventListener("click", function () {
   //   if (!parseInt(rozpocet.value) && parseInt(rozpocet.value) !== 0) {
   //     window.alert("nebyl zadan rozpocet");
   //   }
+
   cenaZapujcky = 0;
   //
   console.log("vypocti clicked");
@@ -24,7 +26,11 @@ btnVypocti.addEventListener("click", function () {
     if (vybranaKola[i].checked) {
       cenaZapujcky += vybranaKola[i].value * vybranaKola[i + 1].value;
       console.log(vybranaKola[i + 1].value);
+      isAtLeastOneBicycleSelected = true;
     }
+  }
+  if (!isAtLeastOneBicycleSelected) {
+    alert("NO bicycle selected");
   }
   console.log(`pred dobou ${cenaZapujcky}`);
   cenaZapujcky = cenaZapujcky * dobaZapujcky.value;
@@ -37,10 +43,12 @@ btnVypocti.addEventListener("click", function () {
   }
   console.log(`cena s nosicen ${cenaZapujcky}`);
 
-  vypis.textContent = `Cena vasi reservace ${cenaZapujcky.toString()} coz ${
-    cenaZapujcky > rozpocet.value ? "neni" : "je"
-  } v souladu s rozpoctem. \n`;
-  vypisCeny.value = cenaZapujcky.toString();
+  if (cenaZapujcky !== 0) {
+    vypis.textContent = `Cena vasi reservace ${cenaZapujcky.toString()} coz ${
+      cenaZapujcky > rozpocet.value ? "neni" : "je"
+    } v souladu s rozpoctem. \n`;
+    vypisCeny.value = cenaZapujcky.toString();
+  }
   //
   if (cenaZapujcky !== 0 || cenaZapujcky <= rozpocet.value) {
     btnRezerve.dis;
@@ -50,12 +58,16 @@ btnVypocti.addEventListener("click", function () {
 
 btnRezerve.addEventListener("click", (e) => {
   console.log(`${email.toString().indexOf("@")}`);
-  if (
-    email.value.toString().indexOf("@") !== -1 ||
-    email.value.toString().indexOf(".") !== -1
-  ) {
+  if (isAtLeastOneBicycleSelected) {
+    if (
+      email.value.toString().indexOf("@") !== -1 ||
+      email.value.toString().indexOf(".") !== -1
+    ) {
+    } else {
+      alert("email is not correct");
+      e.preventDefault();
+    }
   } else {
-    alert("email is not correct");
-    e.preventDefault();
+    alert("No bicycle selected");
   }
 });
